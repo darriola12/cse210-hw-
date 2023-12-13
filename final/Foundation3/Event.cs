c453b188aa5fd21a45f2f8a9b3730e38a48d6967
darriola12 <diego.arriolav1@gmail.com>
2023-12-12 19:35:44 -0600

    cmabio

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,35 @@
+public class Event
+{
+    protected string _title;
+
+    private string _descrip;
+    protected string _date;
+    private string _time;
+    private Address _address;
+
+    public Event(string title, string descrip, string date, string time, Address address)
+    {
+        _title = title;
+        _descrip = descrip;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
+
+    
+    public string TDDTA()//standard
+    {
+        return $"Title: {_title}\nDescription: {_descrip}\nDate: {_date}\nTime: {_time}\nAddress: " + _address.fullAddress();
+    }
+
+    public virtual string detailedInfo()
+    {
+        return TDDTA();
+    }
+
+
+    public virtual string shortDescription()
+    {
+        return $"Title: {_title}\nDate: {_date}";
+    }
+}
